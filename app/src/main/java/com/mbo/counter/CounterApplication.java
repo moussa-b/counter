b99d09aa0ab18825ca577560e197d7b75aa808d3
Moussa Boudamouz <mboudamouz@reseau-delta.ch>
2018-08-09 11:37:34 +0200

    add fabric to the app

--- app/src/main/java/com/mbo/counter/CounterApplication.java
+++ app/src/main/java/com/mbo/counter/CounterApplication.java
@@ -2,8 +2,10 @@ package com.mbo.counter;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.mbo.counter.data.source.ormlite.OrmLiteDataSource;
 import com.mbo.counter.data.source.ormlite.OrmLiteHelper;
+import io.fabric.sdk.android.Fabric;
 
 public class CounterApplication extends Application
 {
@@ -11,6 +13,7 @@ public class CounterApplication extends Application
     public void onCreate()
     {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         OrmLiteDataSource.initialize(new OrmLiteHelper(getApplicationContext()));
     }
 }
