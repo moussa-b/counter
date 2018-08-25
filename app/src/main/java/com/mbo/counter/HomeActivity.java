f7d85226a7f8e0848e582434787d4d85669ac8c0
Moussa Boudamouz <mboudamouz@reseau-delta.ch>
2018-08-26 00:01:49 +0200

    refactor use bottom navigation

---
+++ app/src/main/java/com/mbo/counter/HomeActivity.java
@@ -0,0 +1,164 @@
+package com.mbo.counter;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.mbo.commons.utils.Utils;
+import com.mbo.counter.counter.CounterFragment;
+import com.mbo.counter.counter.CounterPresenter;
+import com.mbo.counter.countergrouplist.CounterGroupListFragment;
+import com.mbo.counter.countergrouplist.CounterGroupListPresenter;
+import com.mbo.counter.counterlist.CounterListFragment;
+import com.mbo.counter.counterlist.CounterListPresenter;
+import com.mbo.counter.data.source.ormlite.OrmLiteDataSource;
+import com.mbo.counter.settings.SettingsFragment;
+import com.mbo.counter.settings.SettingsPresenter;
+
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener
+{
+
+    // https://medium.com/mindorks/essential-guide-for-designing-your-android-app-architecture-mvp-part-1-74efaf1cda40
+    CounterListFragment mCounterListFragment;
+    CounterGroupListFragment mCounterGroupListFragment;
+    CounterFragment mCounterFragment;
+    SettingsFragment mSettingsFragment;
+    boolean mHideMenu = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        mCounterFragment = (CounterFragment) getSupportFragmentManager().findFragmentById(R.id.contentFrame);
+        if (mCounterFragment == null)
+        {
+            // Create the fragment
+            mCounterFragment = CounterFragment.newInstance();
+            Utils.addFragmentToActivity(getSupportFragmentManager(), mCounterFragment, R.id.contentFrame);
+        }
+
+        new CounterPresenter(1, OrmLiteDataSource.getInstance(), mCounterFragment);
+
+        mCounterListFragment = CounterListFragment.newInstance();
+        new CounterListPresenter(OrmLiteDataSource.getInstance(), mCounterListFragment);
+
+        mCounterGroupListFragment = CounterGroupListFragment.newInstance();
+        new CounterGroupListPresenter(OrmLiteDataSource.getInstance(), mCounterGroupListFragment);
+
+        mSettingsFragment = SettingsFragment.newInstance();
+        new SettingsPresenter(mSettingsFragment);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        if (mHideMenu)
+        {
+            for (int i = 0; i < menu.size(); i++)
+                menu.getItem(i).setVisible(false);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        // int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item)
+    {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        switch (item.getItemId())
+        {
+            case R.id.navigation_home:
+                mHideMenu = true;
+                if (mCounterFragment.isAdded())
+                    transaction.show(mCounterFragment);
+                else
+                    Utils.addFragmentToActivity(getSupportFragmentManager(), mCounterFragment, R.id.contentFrame);
+
+                if (mCounterListFragment.isAdded())
+                    transaction.hide(mCounterListFragment);
+
+                if (mCounterGroupListFragment.isAdded())
+                    transaction.hide(mCounterGroupListFragment);
+
+                if (mSettingsFragment.isAdded())
+                    transaction.hide(mSettingsFragment);
+
+                break;
+            case R.id.navigation_counters:
+                mHideMenu = false;
+                if (mCounterListFragment.isAdded())
+                    transaction.show(mCounterListFragment);
+                else
+                    Utils.addFragmentToActivity(getSupportFragmentManager(), mCounterListFragment, R.id.contentFrame);
+
+                if (mCounterFragment.isAdded())
+                    transaction.hide(mCounterFragment);
+
+                if (mCounterGroupListFragment.isAdded())
+                    transaction.hide(mCounterGroupListFragment);
+
+                if (mSettingsFragment.isAdded())
+                    transaction.hide(mSettingsFragment);
+
+                break;
+            case R.id.navigation_folders:
+                mHideMenu = false;
+                if (mCounterGroupListFragment.isAdded())
+                    transaction.show(mCounterGroupListFragment);
+                else
+                    Utils.addFragmentToActivity(getSupportFragmentManager(), mCounterGroupListFragment, R.id.contentFrame);
+
+                if (mCounterFragment.isAdded())
+                    transaction.hide(mCounterFragment);
+
+                if (mCounterListFragment.isAdded())
+                    transaction.hide(mCounterListFragment);
+
+                if (mSettingsFragment.isAdded())
+                    transaction.hide(mSettingsFragment);
+
+                break;
+            case R.id.navigation_settings:
+                mHideMenu = true;
+                if (mSettingsFragment.isAdded())
+                    transaction.show(mSettingsFragment);
+                else
+                    Utils.addFragmentToActivity(getSupportFragmentManager(), mSettingsFragment, R.id.contentFrame);
+
+                if (mCounterFragment.isAdded())
+                    transaction.hide(mCounterFragment);
+
+                if (mCounterListFragment.isAdded())
+                    transaction.hide(mCounterListFragment);
+
+                if (mCounterGroupListFragment.isAdded())
+                    transaction.hide(mCounterGroupListFragment);
+
+                break;
+        }
+
+        transaction.commit();
+        invalidateOptionsMenu();
+        return true;
+    }
+}
